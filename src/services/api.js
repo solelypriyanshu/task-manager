382a88dc7fadb3664daa3b7aff3924eee4b76607
solelypriyanshu <165899885+solelypriyanshu@users.noreply.github.com>
2025-01-20 20:00:18 +0530

    updated api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://server-taskmanager-2v6z.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
